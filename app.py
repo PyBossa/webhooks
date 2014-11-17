631611dbcf7fd16d14a5776b959cbe64eec1254f
Daniel Lombraña González <teleyinex@gmail.com>
2014-11-17 10:40:19 +0100

    Refactor

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import json
+import enki
+import settings
 from flask import Flask, render_template, request
+from analysis import basic
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
@@ -7,8 +10,7 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     else:
-        with open('./static/results.json', 'w') as f:
-            f.write(json.dumps(request.json))
+        basic(**request.json)
         return "OK"
 
 if __name__ == "__main__":
