df868adc30efd1c395df92d944dd92ce1aced0f9
Daniel Lombraña González <teleyinex@gmail.com>
2014-11-17 10:50:47 +0100

    Use queues to update stats.

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template, request
 from analysis import basic
+from redis import Redis
+from rq import Queue
+import settings
+
+
 app = Flask(__name__)
+
+if settings.enable_background_jobs:
+    q = Queue(settings.queue_name, connection=Redis())
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
     else:
-        basic(**request.json)
+        if settings.enable_background_jobs:
+            q.enqueue(basic, **request.json)
+        else:
+            basic(**request.json)
         return "OK"
 
 if __name__ == "__main__":
